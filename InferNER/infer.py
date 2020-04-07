654cda9abf4b5683bc2a2e2e4d8edf6c216ac00a
nickisasound <50883360+nickisasound@users.noreply.github.com>
2020-04-07 10:32:30 -0400

    got raw predictions from bert.

--- InferNER/infer.py
+++ InferNER/infer.py
@@ -4,7 +4,7 @@ import os, time, json
 import torch
 from multi_tasking_transformers.heads import SubwordClassificationHead
 # from multi_tasking_transformers.multitaskers import MultiTaskingBert
-from transformers import BertConfig, BertForTokenClassification
+from transformers import BertConfig, BertForTokenClassification, BertModel
 # Transformers https://huggingface.co/transformers/model_doc/bert.html#bertfortokenclassification
 from tokenizers import BertWordPieceTokenizer
 # Tokenizers https://github.com/huggingface/tokenizers/tree/master/bindings/python
@@ -22,26 +22,29 @@ class Sentence(object):
         self.labels = None # Not needed for inference. torch.tensor([1] * input_ids.size(1)).unqueeze(0) # Batch size 1
 
 class InferNER(object):
-    def __init__(self, head_directory):
-        self.head_directory = head_directory
+    def __init__(self, head_directory, bert_directory):
+        # Set device
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
-        path_to_vocab = os.path.join(head_directory, 'vocab.txt')
-        path_to_head_config = os.path.join(head_directory, 'config.json')
 
-        ## LOAD MODEL
+        ## Load Base model
+        self.bert = BertModel.from_pretrained(bert_directory, from_tf=True)
+
+        ## Load Head
+        path_to_head_config = os.path.join(head_directory, 'SubwordClassificationHead_iepa_gene.json')
+        path_to_vocab = os.path.join(head_directory, 'vocab.txt')
+        self.head_directory = head_directory
         self.head_config = BertConfig.from_pretrained(path_to_head_config)
+
+        # ## Load Tokenizer
         self.tokenizer = BertWordPieceTokenizer(path_to_vocab,
                                                 lowercase=False)
         self.tokenizer.enable_padding(direction='right',
                                       pad_id=0,
                                       max_length=self.head_config.max_position_embeddings)
         config = json.load(open(os.path.join(self.head_directory, "SubwordClassificationHead_iepa_gene.json"), 'rb'))
-        head = SubwordClassificationHead('iepa_gene', labels=config['labels'])
-        head.from_pretrained(self.head_directory)
-        print(head.config)
-        # #
-        # self.bert = BertForTokenClassification.from_pretrained(head_directory,
-                                                               # config=self.head_config)
+        self.head = SubwordClassificationHead('iepa_gene', labels=config['labels'])
+        self.head.from_pretrained(self.head_directory)
+
         # # print('Loaded BERT head, config, and tokenizer')
         # #
         # # self.document = None
@@ -54,9 +57,8 @@ class InferNER(object):
         # TODO write to .ann file
 
     def run_single_example(self, text, ):
-        # head = r"SubwordClassificationHead_iepa_gene_checkpoint_20"
-        head = os.path.basename(self.head_directory)
-        nlp = TransformersLanguage(trf_name=head, meta={"lang": "en"})  # TODO rename
+        head_name = os.path.basename(self.head_directory)
+        nlp = TransformersLanguage(trf_name=head_name, meta={"lang": "en"})  # TODO rename
         nlp.add_pipe(nlp.create_pipe("sentencizer"))
         sentencized_document = nlp(text)
         sentence = str(list(sentencized_document.sents)[0])
@@ -68,17 +70,22 @@ class InferNER(object):
         self.document = sentencized_document
         self.input_ids = input_ids
         self.sentence_encoding = sentence_encoding
-        # self.bert.eval()
-        # with torch.no_grad():
-        #     self.outputs = self.bert(input_ids=input_ids,
-        #                         attention_mask=attention_mask,
-        #                         token_type_ids=token_type_ids,
-        #                         position_ids=None)[0]
-        #     self.predictions = torch.argmax(self.outputs, dim=2)
-        #     label_list = ["LABEL_0", "LABEL_1"]
-        #     print([(token, label_list[prediction]) \
-        #            for token, prediction \
-        #            in zip(self.sentence_encoding.tokens, self.predictions[0].tolist())])
+
+        self.bert.eval()
+        with torch.no_grad():
+            print(f"Predicting {self.head}")
+            predictions =  []
+
+            self.outputs = self.bert(input_ids=input_ids,
+                                attention_mask=attention_mask,
+                                token_type_ids=token_type_ids,
+                                position_ids=None)[0]
+
+            self.predictions = torch.argmax(self.outputs, dim=2)
+            # label_list = ["LABEL_0", "LABEL_1"]
+            # print([(token, label_list[prediction]) \
+            #        for token, prediction \
+            #        in zip(self.sentence_encoding.tokens, self.predictions[0].tolist())])
         #
         #
         # # TODO position ids
@@ -110,9 +117,11 @@ PATH_TO_VOCAB = 'models/vocab.txt'
 # data_dir = 'raw-data'
 # PATH_TO_MODEL = "../models"
 PATH_TO_FILE = r'raw-data/ACS-100/sb3/sb3000723.txt'
+PATH_TO_BASE_MODEL = r'C:/Users/User/nlp/projects/synbiobert/models/biobert_v1.1_pubmed'
 with open(PATH_TO_FILE, encoding='utf8') as f:
     document_as_string = f.read()  # does this work for large documents?
-foo = InferNER(r"C:\Users\User\nlp\projects\synbiobert\models\SubwordClassificationHead_iepa_gene_checkpoint_20")
+foo = InferNER(r"C:\Users\User\nlp\projects\synbiobert\models\SubwordClassificationHead_iepa_gene_checkpoint_20",
+               PATH_TO_BASE_MODEL)
 foo.run_single_example(document_as_string)
 end = time.time()
 print(f'Finished in {end - start:0.2f} seconds')
