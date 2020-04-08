7b83d4e88648d6e25384714516005eb4ed884669
nickisasound <50883360+nickisasound@users.noreply.github.com>
2020-04-08 16:46:47 -0400

    predictions look good. starting document level predictions and conll output.

--- InferNER/infer.py
+++ InferNER/infer.py
@@ -9,6 +9,7 @@ from transformers import BertConfig, BertForTokenClassification, BertModel
 from tokenizers import BertWordPieceTokenizer
 # Tokenizers https://github.com/huggingface/tokenizers/tree/master/bindings/python
 from spacy_transformers import TransformersLanguage
+import pandas as pd
 
 # log = logging.getLogger('root')
 
@@ -27,11 +28,12 @@ class InferNER(object):
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
 
         ## Load Base model
-        self.bert = BertModel.from_pretrained(bert_directory, from_tf=True)
+        self.bert = BertModel.from_pretrained(head_directory, from_tf=False)
 
         ## Load Head
         path_to_head_config = os.path.join(head_directory, 'SubwordClassificationHead_iepa_gene.json')
         path_to_vocab = os.path.join(head_directory, 'vocab.txt')
+        # path_to_vocab = os.path.join(head_directory, 'vocab.txt')
         self.head_directory = head_directory
         self.head_config = BertConfig.from_pretrained(path_to_head_config)
 
@@ -43,7 +45,7 @@ class InferNER(object):
                                       max_length=self.head_config.max_position_embeddings)
         config = json.load(open(os.path.join(self.head_directory, "SubwordClassificationHead_iepa_gene.json"), 'rb'))
         self.head = SubwordClassificationHead('iepa_gene', labels=config['labels'])
-        self.head.from_pretrained(self.head_directory)
+        print(self.head.from_pretrained(self.head_directory))
 
         # # print('Loaded BERT head, config, and tokenizer')
         # #
@@ -56,32 +58,57 @@ class InferNER(object):
         # # # TODO predict each sentence (in batches?)
         # TODO write to .ann file
 
-    def run_single_example(self, text, ):
+    def run_single_example(self, text):
         head_name = os.path.basename(self.head_directory)
         nlp = TransformersLanguage(trf_name=head_name, meta={"lang": "en"})  # TODO rename
         nlp.add_pipe(nlp.create_pipe("sentencizer"))
         sentencized_document = nlp(text)
-        sentence = str(list(sentencized_document.sents)[0])
-        sentence_encoding = self.tokenizer.encode(sentence)
-        # PREPARE MODEL INPUT
-        input_ids = torch.tensor([sentence_encoding.ids], dtype=torch.long, device=self.device)
-        attention_mask = torch.tensor([sentence_encoding.attention_mask], dtype=torch.long, device=self.device)
-        token_type_ids = torch.tensor([sentence_encoding.type_ids], dtype=torch.long, device=self.device)
-        self.document = sentencized_document
-        self.input_ids = input_ids
-        self.sentence_encoding = sentence_encoding
+        # self.sentence = str(list(sentencized_document.sents)[0]) # TODO break off into a separate method to use one sentence
+        # self.sentence = "The Ca2+ ionophore , A23187 or ionomycin , mimicked the effect of AVP , whereas the protein kinase C ( PKC ) activator , TPA , only induced a slight increase in AA release"
+        self.sentence = r"Activating mutations in BRAF have been reported in 5–15 % of colorectal carcinomas ( CRC ) , with by far the most common mutation being a 1796T to A transversion leading to a V600E substitution [1-3] .  The BRAF V600E hotspot mutation is strongly associated with the microsatellite instability ( MSI+ ) phenotype but is mutually exclusive with KRAS mutations [4-7] ."
+        self.sentence_encoding = self.tokenizer.encode(self.sentence)
 
+        # PREPARE MODEL INPUT
+        input_ids = torch.tensor([self.sentence_encoding.ids], dtype=torch.long, device=self.device)
+        attention_mask = torch.tensor([self.sentence_encoding.attention_mask], dtype=torch.long, device=self.device)
+        token_type_ids = torch.tensor([self.sentence_encoding.type_ids], dtype=torch.long, device=self.device)
+        self.document = sentencized_document
+        self.tokens = self.sentence_encoding.tokens
+        self.spans = self.sentence_encoding.offsets
+        self.input_ids = input_ids
+
+        # RUN EXAMPLE THROUGH BERT
         self.bert.eval()
         with torch.no_grad():
             print(f"Predicting {self.head}")
-            predictions =  []
-
-            self.outputs = self.bert(input_ids=input_ids,
+            self.bert_outputs = self.bert(input_ids=input_ids,
                                 attention_mask=attention_mask,
-                                token_type_ids=token_type_ids,
+                                # token_type_ids=token_type_ids,
+                                token_type_ids=None,
                                 position_ids=None)[0]
+            self.subword_scores = self.head(self.bert_outputs)[0]
+            # self.predicted_label_keys = self.subword_scores.max(dim=2).indices[0]
+            self.predicted_label_keys = self.subword_scores.max(2)[1][0]  # to run in batch mode, [0] to i
 
-            self.predictions = torch.argmax(self.outputs, dim=2)
+            self.labels = sorted(self.head.config.labels)
+            self.predicted_labels = [self.labels[label_key] for label_key in self.predicted_label_keys]
+
+            sentence_subword_length = self.sentence_encoding.special_tokens_mask.count(0)
+            token_mask = self.sentence_encoding.special_tokens_mask
+
+            subwords_idx = [index_of_subword for index_of_subword, mask in enumerate(token_mask) if mask==0]
+
+            # Print tokenized sentence
+            self.output_tokens = [self.sentence_encoding.tokens[i] for i in subwords_idx]
+            # Print subword spans
+            self.output_spans = [str(self.sentence_encoding.offsets[i]) for i in subwords_idx]
+            # Print labels
+            self.output_labels = [self.predicted_labels[i] for i in subwords_idx]
+            self.output_table = pd.DataFrame.from_dict(
+                {'tokens': self.output_tokens, 'labels': self.output_labels, 'spans': self.output_spans})
+
+
+
             # label_list = ["LABEL_0", "LABEL_1"]
             # print([(token, label_list[prediction]) \
             #        for token, prediction \
@@ -129,3 +156,5 @@ print(f'Finished in {end - start:0.2f} seconds')
 # TODO Check for 512 token max + padding
 # TODO predict and infer
 
+# "The Ca2+ ionophore , A23187 or ionomycin , mimicked the effect of AVP , whereas the protein kinase C ( PKC ) activator , TPA , only induced a slight increase in AA release"
+r"Activating mutations in BRAF have been reported in 5–15 % of colorectal carcinomas ( CRC ) , with by far the most common mutation being a 1796T to A transversion leading to a V600E substitution [1-3] .  The BRAF V600E hotspot mutation is strongly associated with the microsatellite instability ( MSI+ ) phenotype but is mutually exclusive with KRAS mutations [4-7] ."
