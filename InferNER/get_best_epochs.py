1e36faebc0eef70983ffb5815733f9b2e4848409
nick <rodriguezne2@vcu.edu>
2020-05-29 04:59:55 -0400

    debugging eval

--- InferNER/get_best_epochs.py
+++ InferNER/get_best_epochs.py
@@ -4,19 +4,21 @@ import gin
 
 def get_filenames_best_epoch(performances):
     epoch_of_best_mtl_performance = {}
+    filenames_best_epoch = {}
     for dataset, task, metric, my_performance, other_performance in performances:
         epoch_of_best_mtl_performance[dataset] = my_performance.index(max(my_performance))
-        print(f"SubwordClassificationHead_{dataset}_checkpoint_{epoch_of_best_mtl_performance[dataset]}")
+        filenames_best_epoch[dataset] = f"SubwordClassificationHead_{dataset}_checkpoint_{epoch_of_best_mtl_performance[dataset]}"
+    return filenames_best_epoch
 
 
-def get_best_epoch(run_id:str):
-    # huner_bert_finetuning = MLFlowRun(tracking_uri='http://localhost:1730/',
-    #                                      experiment_name='task_finetuning',
-    #                                      run_id='a3e092285a34480ca1c1b2c114a7967d'
-    #                                      )
-    huner_bert_finetuning = MLFlowRun(tracking_uri='http://localhost:1730/',
-                                      experiment_name='task_finetuning',
-                                      run_id=run_id
+@gin.configurable
+def get_best_epoch(ml_flow_directory,
+                   run_directory,
+                   experiment_name):
+
+    huner_bert_finetuning = MLFlowRun(tracking_uri=ml_flow_directory,
+                                      experiment_name=experiment_name,
+                                      run_id=run_directory
                                       )
     performances = huner_bert_finetuning.compare_against_run(huner_bert_finetuning, 'ner')
     epoch_of_best_mtl_performance = {}
@@ -26,8 +28,9 @@ def get_best_epoch(run_id:str):
     print()
     for dataset, task, metric, my_performance, other_performance in performances:
         print(f"{dataset}_{task}\t{max(my_performance)}\t{max(other_performance)}")
+    return get_filenames_best_epoch(performances)
 
-    get_filenames_best_epoch(performances)
+
 
 if __name__ == '__main__':
     get_best_epoch()
